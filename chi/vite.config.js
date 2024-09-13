e0398aa77f8bd71bbafc6876e5512951d5c52527
chi <a27976566@gmail.com>
2024-09-13 18:09:20 +0800

    add test_vue

--- chi/vite.config.js
+++ chi/vite.config.js
@@ -1,10 +1,11 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import tailwindcss from 'tailwindcss'; 
-import autoprefixer from 'autoprefixer'; 
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
+  base: '/test_vue/',
   plugins: [vue()],
   resolve: {
     alias: {
